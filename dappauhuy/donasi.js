904319d02589c6056fe8fc115fe96a3470bd9a16
dappauhuy <79284980+dappauhuy@users.noreply.github.com>
2021-03-12 00:20:21 +0700

    Add files via upload

---
+++ dappauhuy/donasi.js
@@ -0,0 +1,17 @@
+const donasi = () => { 
+	return `       
+┏━━━━━━━━━━━━━━━━━━━━
+┃        *_DONASI_*  
+┣━━━━━━━━━━━━━━━━━━━━
+┣━⊱❉ DONASI BOSS ❉⊰━━✿
+┃  
+┣━⊱ *PULSA*
+┣⊱ 0858-7633-0812
+┣━⊱ *DANA*
+┣⊱ 0858-7633-0812
+┃
+┃Gunain doank, kaga donasi
+┗━━━━━━━━━━━━━━━━━━━━`
+}
+
+exports.donasi = donasi
---
+++ dappauhuy/sewabot.js
@@ -0,0 +1,17 @@
+const sewabot = () => { 
+	return `
+Sewa BOT
+
+10k = ganti owner, ganti nomer owner, ganti nama bot, no apikey
+25k = ganti owner, ganti nomer owner, ganti nama bot, ada apikey
+SELAMA 3 HARI
+
+20k = ganti owner, ganti nomer owner, ganti nama bot, no apikey
+50k = ganti owner, ganti nomer owner, ganti nama bot, ada apikey
+SELAMA 1 MINGGU
+
+minat? hubungi wa.me/6285876330812
+pembayaran via Pulsa/Dana
+`
+}
+exports.sewabot = sewabot
