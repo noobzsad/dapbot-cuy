911c573b836adb5ac91a76f12677a694d528330f
noobzsad <77076175+noobzsad@users.noreply.github.com>
2021-03-19 15:29:13 +0700

    Add files via upload

--- dapxuhy.js
+++ dapxuhy.js
@@ -1227,7 +1227,7 @@ break
 				ct = body.slice(9)
 				reply(ind.wait())
 				ct = await getBuffer(`http://lolhuman.herokuapp.com/api/ephoto1/hologram3d?apikey=WEMPYGANSS&text=${ct}`)
-				itsmeiky.sendMessage(from, ct, image, {caption: 'Nih kak udah jadi..', quoted: iky})
+				dappa.sendMessage(from, ct, image, {caption: 'Nih kak udah jadi..', quoted: mek})
 				await limitAdd(sender)
 				break
 		case 'textbyname':			
@@ -4348,67 +4348,71 @@ break
 					fs.writeFileSync('./dapp/user/banned.json', JSON.stringify(ban))
 					reply(`Nomor wa.me/${bnnd} telah di unban!`)
 					break
-				case 'stiker': 
-				case 'sticker':
-				case 's':
-				     if (!isRegistered) return reply( ind.noregis())
-				    if (isLimit(sender)) return reply(ind.limitend(pusname))
-				if (isBanned) return reply('Maaf kamu sudah terbenned!')
-					await limitAdd(sender)
-					if ((isMedia && !mek.message.videoMessage || isQuotedImage) && args.length == 0) {
-						const encmedia = isQuotedImage ? JSON.parse(JSON.stringify(mek).replace('quotedM','m')).message.extendedTextMessage.contextInfo : mek
-						const media = await client.downloadAndSaveMediaMessage(encmedia)
-						ran = getRandom('.webp')
-						await ffmpeg(`./${media}`)
-							.input(media)
-							.on('start', function (cmd) {
-								console.log(`Started : ${cmd}`)
-							})
-							.on('error', function (err) {
-								console.log(`Error : ${err}`)
-								fs.unlinkSync(media)
-								reply(ind.stikga())
-							})
-							.on('end', function () {
-								console.log('Finish')
-								buffer = fs.readFileSync(ran)
-								dappa.sendMessage(from, buffer, sticker, {quoted: mek})
-								fs.unlinkSync(media)
-								fs.unlinkSync(ran)
-							})
-							.addOutputOptions([`-vcodec`,`libwebp`,`-vf`,`scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
-							.toFormat('webp')
-							.save(ran)
-					} else if ((isMedia && mek.message.videoMessage.seconds < 11 || isQuotedVideo && mek.message.extendedTextMessage.contextInfo.quotedMessage.videoMessage.seconds < 11) && args.length == 0) {
-						const encmedia = isQuotedVideo ? JSON.parse(JSON.stringify(mek).replace('quotedM','m')).message.extendedTextMessage.contextInfo : mek
-						const media = await client.downloadAndSaveMediaMessage(encmedia)
-						ran = getRandom('.webp')
-						reply(ind.wait())
-						await ffmpeg(`./${media}`)
-							.inputFormat(media.split('.')[1])
-							.on('start', function (cmd) {
-								console.log(`Started : ${cmd}`)
-							})
-							.on('error', function (err) {
-								console.log(`Error : ${err}`)
-								fs.unlinkSync(media)
-								tipe = media.endsWith('.mp4') ? 'video' : 'gif'
-								reply(ind.stikga())
-							})
-							.on('end', function () {
-								console.log('Finish')
-								buffer = fs.readFileSync(ran)
-								dappa.sendMessage(from, buffer, sticker, {quoted: mek})
-								fs.unlinkSync(media)
-								fs.unlinkSync(ran)
-							})
-							.addOutputOptions([`-vcodec`,`libwebp`,`-vf`,`scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
-							.toFormat('webp')
-							.save(ran)
-							} else {
-						reply(`Kirim gambar dengan caption ${prefix}sticker atau reply/tag gambar`)
-					}
-					break
+				case 'stickergif':
+      case 'stikergif':
+      case 'sgif':
+      case 'stiker':
+      case 'sticker':
+      case 's':
+        // Fix Bug By OzanDesu				
+        if (!isRegistered) return reply(ind.noregis())
+        if (isLimit(sender)) return reply(ind.limitend(pusname))
+        if (isBanned) return reply('Maaf kamu sudah terbenned!')
+        await limitAdd(sender)
+        if ((isMedia && !mek.message.videoMessage || isQuotedImage) && args.length == 0) {
+          const encmedia = isQuotedImage ? JSON.parse(JSON.stringify(mek).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo : mek
+          const media = await dappa.downloadAndSaveMediaMessage(encmedia)
+          ran = getRandom('.webp')
+          await ffmpeg(`./${media}`)
+            .input(media)
+            .on('start', function(cmd) {
+              console.log(`Started : ${cmd}`)
+            })
+            .on('error', function(err) {
+              console.log(`Error : ${err}`)
+              fs.unlinkSync(media)
+              reply(ind.stikga())
+            })
+            .on('end', function() {
+              console.log('Finish')
+              buffer = fs.readFileSync(ran)
+              dappa.sendMessage(from, buffer, sticker, { quoted: mek })
+              fs.unlinkSync(media)
+              fs.unlinkSync(ran)
+            })
+            .addOutputOptions([`-vcodec`, `libwebp`, `-vf`, `scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
+            .toFormat('webp')
+            .save(ran)
+        } else if ((isMedia && mek.message.videoMessage.seconds < 11 || isQuotedVideo && mek.message.extendedTextMessage.contextInfo.quotedMessage.videoMessage.seconds < 11) && args.length == 0) {
+          const encmedia = isQuotedVideo ? JSON.parse(JSON.stringify(mek).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo : mek
+          const media = await dappa.downloadAndSaveMediaMessage(encmedia)
+          ran = getRandom('.webp')
+          reply(ind.wait())
+          await ffmpeg(`./${media}`)
+            .inputFormat(media.split('.')[1])
+            .on('start', function(cmd) {
+              console.log(`Started : ${cmd}`)
+            })
+            .on('error', function(err) {
+              console.log(`Error : ${err}`)
+              fs.unlinkSync(media)
+              tipe = media.endsWith('.mp4') ? 'video' : 'gif'
+              reply(ind.stikga())
+            })
+            .on('end', function() {
+              console.log('Finish')
+              buffer = fs.readFileSync(ran)
+              dappa.sendMessage(from, buffer, sticker, { quoted: mek })
+              fs.unlinkSync(media)
+              fs.unlinkSync(ran)
+            })
+            .addOutputOptions([`-vcodec`, `libwebp`, `-vf`, `scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
+            .toFormat('webp')
+            .save(ran)
+        } else {
+          reply(`Kirim gambar dengan caption ${prefix}sticker atau reply/tag gambar`)
+        }
+        break
 				case 'tts':
 				if (!isRegistered) return reply(ind.noregis())
 		if (isBanned) return reply(ind.baned())
